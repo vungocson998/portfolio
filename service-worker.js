eacedab392ea4488da6cae670977c6e511f76d36
vungocson <vungocson998@gmail.com>
2020-03-19 11:38:20 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.237094e070fb2a4eb66c98e72ba044f4.js"
+  "/portfolio/precache-manifest.0df19bf4a0aeda57b2a2d62ed6917e24.js"
 );
 
 self.addEventListener('message', (event) => {
